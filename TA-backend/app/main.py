b61020ab2659c124c2112dff724d82d976f6cb6b
Eugeniusz <von1we98@gmai.com>
2026-08-17 21:19:21 +0200

    first commit

---
+++ TA-backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "Training Assystant API"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "OK"
+    }
